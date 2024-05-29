f160e37a9896a7785e323bbdb00d6720123323a0
bl0up <ambroise.m2@gmail.com>
2024-05-29 15:11:49 +0200

    feat: improve coverage

--- test/SoulboundToken.t.sol
+++ test/SoulboundToken.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.26;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Soulbound} from "../contracts/SoulboundToken.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract SoulboundTokenTest is Test {
     Soulbound soulbound;
@@ -39,6 +40,10 @@ contract SoulboundTokenTest is Test {
         );
     }
 
+    function testSetApprovalForAll() public {
+        soulbound.setApprovalForAll(address(3), true);
+    }
+
     function testDefaultMinterRole() public {
         soulbound.grantRole(soulbound.MINTER_ROLE(), address(1));
         assertTrue(soulbound.hasRole(soulbound.MINTER_ROLE(), address(1)));
@@ -53,6 +58,16 @@ contract SoulboundTokenTest is Test {
         soulbound.grantRole(soulbound.MINTER_ROLE(), address(this));
         soulbound.mint(2, address(2));
         assertEq(soulbound.balanceOf(address(2), 1), 2);
+        assertEq(soulbound.getCurrentTokenId(), 1);
+    }
+
+    function testBurnFunction() public {
+        soulbound.grantRole(soulbound.MINTER_ROLE(), address(this));
+        soulbound.mint(2, address(2));
+        assertEq(soulbound.balanceOf(address(2), 1), 2);
+        vm.prank(address(2));
+        soulbound.burn(1, 1);
+        assertEq(soulbound.balanceOf(address(2), 1), 1);
         assertEq(soulbound.getCurrentTokenId(), 1);
     }
 
@@ -125,5 +140,13 @@ contract SoulboundTokenTest is Test {
         assertEq(soulbound.balanceOf(address(2), 1), 0);
         assertEq(soulbound.balanceOf(address(2), 2), 0);
         assertEq(soulbound.balanceOf(address(2), 3), 0);
+    }
+
+    function testSupportsInterface() public {
+        // ERC165 Interface ID for ERC1155
+        bytes4 interfaceIdERC1155 = type(IERC1155).interfaceId;
+
+        bool supportsERC1155 = soulbound.supportsInterface(interfaceIdERC1155);
+        assertTrue(supportsERC1155, "should support ERC165 interface");
     }
 }
